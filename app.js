08ff4bc5c9bcdf2703b256f7d34092514b3d5a08
Saif Kbishi <Saifkbishi@gmail.com>
2021-03-11 23:48:40 +0200

    can edit the table

--- app.js
+++ app.js
@@ -2,16 +2,28 @@ console.log('CapsulesAPI_CRUD');
 
 const BaseEndpoint= 'https://apple-seeds.herokuapp.com/api/users/';
 const personDataArray = [];
-//const personExtraDetailsArray = [];
 const toolsBar = document.querySelector('.toolsBar');
 const personData = document.querySelector('.personData');
 const table = document.createElement('table');
-const tableHeadStr = `<thead class="thead"><tr><th>id</th><th>First Name</th><th>Last Name</th><th>Capsule</th><th>Age</th><th>City</th><th>Gender</th>
-<th>Hobby</th><th></th><th></th></tr></thead>`;
+const tableHeadStr = `<thead class="thead"><tr><th>id</th><th>First Name</th><th>Last Name</th><th>Capsule</th><th>Age</th><th>City</th><th>Gender</th><th>Hobby</th><th></th><th></th></tr></thead>`;
 const tbody = document.createElement('tbody');
+tbody.classList.add('mytable');
+let rowState={
+ rowIndex: 0,
+ tempRow:'',
+ inEditMode: false,
+}
+
+
+function createDropDownOption(elemType, textcontent, value){
+ let newElement = document.createElement(elemType);
+ newElement.textContent = textcontent;
+ newElement.value = value;
+ return newElement;
+}
 
 createToolBarMenu();
-//getPersonDataFromApi();
+getPersonDataFromApi();
 
 //generic function for fetching URL
 async function fetchAnyURL(url){
@@ -23,6 +35,14 @@ async function fetchAnyURL(url){
  }
  catch(error){console.log(`${error}, error fetching data from ${url}`);}
 }//fetchAnyURL
+
+async function getPersonExtraDetails(id){
+ try{
+  const personExtraDetails = await fetchAnyURL(`${BaseEndpoint}${id}`);
+  return personExtraDetails;  
+ }
+ catch(error){console.log(`${error} ,Could not fetch person with id=${id} extra data from ${BaseEndpoint}`);}
+}//getPersonDetails
 
 async function getPersonDataFromApi(){
  try{
@@ -39,93 +59,114 @@ async function getPersonDataFromApi(){
      age: personExtraDetails.age,
      hobby: personExtraDetails.hobby
    };
-   //console.log(dataObj);
    personDataArray.push(dataObj);
-   }  
-   //console.log(personDataArray);
+   }
   diplayData();
  } 
  catch(error){console.log(`${error}, Could not fetch person data from ${BaseEndpoint}`);}
 }//getPersonDataFromApi
 
-async function getPersonExtraDetails(id){
- try{
-  const personExtraDetails = await fetchAnyURL(`${BaseEndpoint}${id}`);
-  return personExtraDetails;  
- }
- catch(error){console.log(`${error} ,Could not fetch person with id=${id} extra data from ${BaseEndpoint}`);}
-}//getPersonDetails
+function createToolBarMenu(){
+ const searchLabel = document.createElement('lable');
+ searchLabel.textContent = 'Search: ';
+ const searchField = document.createElement('input');
+ searchField.type = 'search';
+ searchField.id = 'searchSite';
+ const dropdownMenu = document.createElement('select');
+ //const elemName = createDropDownOption(elemType, textcontent, value, id, class);
+ const optionFN = createDropDownOption('option', 'First Name', 'firstname');
+ const optionLN = createDropDownOption('option', 'Last Name', 'lastname');
+ const optionAge = createDropDownOption('option', 'Age', 'age');
+ const optionCapsule = createDropDownOption('option', 'Capsule', 'capsule');
+ const optionGender = createDropDownOption('option', 'Gender', 'gender');
+ const optionHobby = createDropDownOption('option', 'Hobby', 'hobby');
+ const optionCity = createDropDownOption('option', 'City', 'city');
+
+ toolsBar.insertAdjacentElement('afterbegin',searchLabel);
+ searchLabel.insertAdjacentElement('afterend',searchField);
+ searchField.insertAdjacentElement('afterend',dropdownMenu);
+
+ dropdownMenu.insertAdjacentElement('beforeend',optionFN);
+ dropdownMenu.insertAdjacentElement('beforeend',optionLN);
+ dropdownMenu.insertAdjacentElement('beforeend',optionAge);
+ dropdownMenu.insertAdjacentElement('beforeend',optionCapsule);
+ dropdownMenu.insertAdjacentElement('beforeend',optionGender);
+ dropdownMenu.insertAdjacentElement('beforeend',optionHobby);
+ dropdownMenu.insertAdjacentElement('beforeend',optionCity);
+ 
+}//createToolBarMenu
+
 
 function diplayData(){
+ hideAnimation();
  try{
   personData.insertAdjacentElement('afterbegin', table);
   table.insertAdjacentHTML('afterbegin', tableHeadStr);
-  //tableHeadStr.classList.add('sadsad')
   let tableHead = document.querySelector('.thead');
-  console.log('thead', tableHead);
   tableHead.insertAdjacentElement('afterend', tbody);
  
   for(let i=0; i<personDataArray.length; i++){
    //tbody.insertAdjacentHTML('beforeend',displayARow(i));
    tbody.insertAdjacentHTML('beforeend',`<tr>
    <td>${personDataArray[i].id}</td>
-   <td>${personDataArray[i].firstName}</td>
-   <td>${personDataArray[i].lastName}</td>
-   <td>${personDataArray[i].capsule}</td>
-   <td>${personDataArray[i].age}</td>
-   <td>${personDataArray[i].city}</td>
-   <td>${personDataArray[i].gender}</td>
-   <td>${personDataArray[i].hobby}</td>
-   <td><button class="editBtn">Edit</button></td>
-   <td><button class="deleteBtn">Delete</button></td>
+   <td data-type="fname">${personDataArray[i].firstName}</td>
+   <td data-type="lname">${personDataArray[i].lastName}</td>
+   <td data-type="capsule">${personDataArray[i].capsule}</td>
+   <td data-type="age">${personDataArray[i].age}</td>
+   <td data-type="city">${personDataArray[i].city}</td>
+   <td data-type="gender">${personDataArray[i].gender}</td>
+   <td data-type="hobby">${personDataArray[i].hobby}</td>
+   <td data-type="editBtn"><button class="editBtn btn">Edit</button></td>
+   <td data-type="deleteBtn"><button class="deleteBtn btn">Delete</button></td>
   </tr>`);
-  }
- }catch(error){console.log(`${error}, could not display table`);}
- 
+  }//for
+  const allEditBtns = document.querySelectorAll('.editBtn');  
+  const allDeleteBtns = document.querySelectorAll('.deleteBtn');
+  //console.log('allEditBtns: ',allEditBtns);
+  editDeleteBtnsAddEventListeners(allEditBtns, allDeleteBtns);
+ }catch(error){console.log(`${error}, could not display table`);} 
 }//diplayData
 
-function createToolBarMenu(){
- const searchLabel = document.createElement('lable');
- searchLabel.textContent = 'Search: ';
- const searchField = document.createElement('input');
- searchField.type='search';
- searchField.id='searchSite';
- const dropdownMenu = document.createElement('select');
- const optionFN = document.createElement('option');
- optionFN.textContent ='First Name';
- const optionLN = document.createElement('option');
- optionLN.textContent='Last Name';
- /* const optionFN = document.createElement('option');
- const optionFN = document.createElement('option');
- const optionFN = document.createElement('option');
-*/
+function editDeleteBtnsAddEventListeners(allEditBtns, allDeleteBtns){
+ allEditBtns.forEach((editBtnElem,index) => {
+  //console.log(editBtnElem);
+  //editBtnElem.addEventListener('click',()=> editRow(index));
+  editBtnElem.addEventListener('click',()=> editRow(index));  
+ });
 
- toolsBar.insertAdjacentElement('afterbegin',searchLabel);
- searchLabel.insertAdjacentElement('afterend',searchField);
- searchField.insertAdjacentElement('afterend',dropdownMenu);
- dropdownMenu.insertAdjacentElement('beforeend',optionFN);
- dropdownMenu.insertAdjacentElement('beforeend',optionLN);
- 
+ allDeleteBtns.forEach((DeleteBtnElem,index) => {
+  //console.log(DeleteBtnElem);
+  DeleteBtnElem.addEventListener('click',()=> deletRow(index));
+ });
+}//editDeleteBtnsAddEventListeners
 
+function editRow(index){
+ console.log('index: ', index);
+ let rowToEdit = tbody.rows[index];
+ tempRow = rowToEdit;
+ //console.log(tbody.rows[index]);
+ console.log('rowToEdit: ',rowToEdit);
+ //tbody.rows[index].contentEditable= "true";
+ //rowToEdit.contentEditable= "true";
+ rowToEdit.classList.add('noBorderForRow');
+ let rowElems = rowToEdit.cells;
+ for(let i=1; i<rowElems.length-2; i++){
+  rowElems[i].contentEditable= "true";
+  rowElems[i].classList.add('cellBorder');
+  //console.log(rowElems[i]);
+ }
+
+ //console.log(tbody);
+}//editRow
+
+function deletRow(index){
+ console.log('index: ', index);
+ let rowToDelete = tbody.rows[index];
+ rowToDelete.remove();
+}//deletRow
+
+
+function hideAnimation(){
+ let animation = document.querySelector('.lds-spinner');
+ animation.style.display= 'none';
 }
-
-
-/* function displayARow(index){
- try{
-  let i =index;
-  let aPersonRecord = `<tr>
-  <td>${personDataArray[i].id}</td>
-  <td>${personDataArray[i].firstName}</td>
-  <td>${personDataArray[i].lastName}</td>
-  <td>${personDataArray[i].capsule}</td>
-  <td>${personDataArray[i].age}</td>
-  <td>${personDataArray[i].city}</td>
-  <td>${personDataArray[i].gender}</td>
-  <td>${personDataArray[i].hobby}</td>
-  <td>EDIT button</td>
-  <td>delete button</td>
- </tr>`;
- }catch(error){`${error} not able to to display a row with index: ${index}`}
- 
-//return aPersonRecord;
-} */
